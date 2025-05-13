0a5e5e2191a830b781a0659abf8a9e871c5dae97
anjasfedo <fedoaf98@gmail.com>
2025-05-13 19:50:06 +0700

    feat: change dice assets

--- diceData.js
+++ diceData.js
@@ -1,8 +1,8 @@
 export const diceData = {
-  1: "./images/dice1.png",
-  2: "./images/dice2.png",
-  3: "./images/dice3.png",
-  4: "./images/dice4.png",
-  5: "./images/dice5.png",
-  6: "./images/dice6.png",
+  1: "./images/dice2_1.png",
+  2: "./images/dice2_2.png",
+  3: "./images/dice2_3.png",
+  4: "./images/dice2_4.png",
+  5: "./images/dice2_5.png",
+  6: "./images/dice2_6.png",
 };
